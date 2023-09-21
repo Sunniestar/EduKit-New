abcaf0e9caf0250931a4b4a609b49e63b9e580c2
Sunniestar <143650691+Sunniestar@users.noreply.github.com>
2023-09-21 11:32:28 -0500

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,6 @@
 importScripts("./uv/uv.bundle.js");
 importScripts("./uv/uv.config.js");
 importScripts("./uv/uv.sw.js");
-import("./uv/uv.bundle.js");
-import("./uv/uv.config.js");
-import("./uv/uv.sw.js");
 
 const sw = new UVServiceWorker();
 
